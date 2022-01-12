f16528835ec410286fda5a7b49d3f65add3cb8dc
Omri Dolberg <88512557+Ukiplezet@users.noreply.github.com>
2022-01-13 00:06:13 +0200

    m2:WIP-i wanna sleep

--- Client/src/Pages/Map.js
+++ Client/src/Pages/Map.js
@@ -5,28 +5,28 @@ import * as tt from "@tomtom-international/web-sdk-maps";
 import * as ttapi from "@tomtom-international/web-sdk-services";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import { convertToPoints, drawRoute, addDestination } from "../Utils/mapFuncs";
-import GpsArrow from "../media/gps-arrow-orange.png"
+import GpsArrow from "../media/gps-arrow-orange.png";
 
 function Map() {
-  const mapEl = useRef()
-  const [map, setMap] = useState({})
+  const mapEl = useRef();
+  const [map, setMap] = useState({});
   const [routeInfo, setRouteInfo] = useState({});
   const [latitude, setLatitude] = useState(32.052797); // set to current location
   const [longitude, setLongitude] = useState(34.772238);
 
   useEffect(() => {
-    const origin = { lng: longitude, lat: latitude }
+    const origin = { lng: longitude, lat: latitude };
     const destinations = [];
 
     let tomtomMap = tt.map({
-        key: process.env.REACT_APP_TOMTOM_API_KEY,
-        container: mapEl.current,
-        stylesVisibility: {
-          trafficIncidents: true,
-          trafficFlow: true
-        },
-        center: [longitude, latitude],
-        zoom: 15
+      key: process.env.REACT_APP_TOMTOM_API_KEY,
+      container: mapEl.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 15,
     });
     setMap(tomtomMap);
 
@@ -42,24 +42,23 @@ function Map() {
         draggable: true,
         element: pin,
       })
-      .setLngLat([longitude, latitude])
-      .addTo(tomtomMap)
+        .setLngLat([longitude, latitude])
+        .addTo(tomtomMap);
 
       marker.on("dragend", () => {
-        const lngLat = marker.getLngLat()
-        setLongitude(lngLat.lng)
+        const lngLat = marker.getLngLat();
+        setLongitude(lngLat.lng);
         setLatitude(lngLat.lat);
-      })
+      });
 
       // marker.setPopup(popUp).togglePopup()
-    }
-    addMarker()
-
+    };
+    addMarker();
 
     const sortDestinations = (locations) => {
-      const destinationPoints = locations.map(destination => {
-        return convertToPoints(destination)
-      })
+      const destinationPoints = locations.map((destination) => {
+        return convertToPoints(destination);
+      });
 
       const callParams = {
         key: process.env.REACT_APP_TOMTOM_API_KEY,
@@ -67,51 +66,50 @@ function Map() {
         origins: [convertToPoints(origin)],
       };
 
-      return new Promise((resolve, reject)=> {
-        ttapi.services.matrixRouting(callParams)
-        .then((matrixAPIResults) => {
-          const results = matrixAPIResults.matrix[0]
+      return new Promise((resolve, reject) => {
+        ttapi.services.matrixRouting(callParams).then((matrixAPIResults) => {
+          const results = matrixAPIResults.matrix[0];
           const resultsArr = results.map((result, index) => {
             return {
               location: locations[index],
               drivingTime: result.response.routeSummary.travelTimeInSeconds,
-            }
+            };
           });
           resultsArr.sort((a, b) => {
-            return a.drivingTime - b.drivingTime
-          })
-          const sortedLocations = resultsArr.map(result => {
-            return result.location
-          })
-          resolve(sortedLocations)
-        })
-      })
-    }
+            return a.drivingTime - b.drivingTime;
+          });
+          const sortedLocations = resultsArr.map((result) => {
+            return result.location;
+          });
+          resolve(sortedLocations);
+        });
+      });
+    };
 
     const recalculateRoutes = () => {
-      sortDestinations(destinations)
-      .then((sorted) => {
-        sorted.unshift(origin)
-        ttapi.services.calculateRoute({
-          key: process.env.REACT_APP_TOMTOM_API_KEY,
-          locations: sorted,
-        })
-        .then(routeData => {
-          setRouteInfo(routeData.routes[0].summary)
-          const geoJson = routeData.toGeoJson()
-          drawRoute(geoJson, tomtomMap)
-        })
-      })
-    }
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin);
+        ttapi.services
+          .calculateRoute({
+            key: process.env.REACT_APP_TOMTOM_API_KEY,
+            locations: sorted,
+          })
+          .then((routeData) => {
+            setRouteInfo(routeData.routes[0].summary);
+            const geoJson = routeData.toGeoJson();
+            drawRoute(geoJson, tomtomMap);
+          });
+      });
+    };
 
     tomtomMap.on("click", (e) => {
       destinations.push(e.lngLat);
       addDestination(e.lngLat, tomtomMap);
-      recalculateRoutes()
+      recalculateRoutes();
     });
 
     return () => tomtomMap.remove();
-  }, [longitude, latitude])
+  }, [longitude, latitude]);
 
   return (
     <Col
@@ -122,9 +120,10 @@ function Map() {
     >
       <OnHoverScrollContainer>
         <Col>Where to?</Col>
-        
-        {map && <>
-          {/* <Col className="search-bar">
+
+        {map && (
+          <>
+            {/* <Col className="search-bar">
             <input
               type="text"
               id="latitude"
@@ -140,8 +139,9 @@ function Map() {
               onChange={(e) => { setLongitude(e.target.value) }}
             />
           </Col> */}
-          <div ref={mapEl} className="map" />
-        </>}
+            <div ref={mapEl} className="map" />
+          </>
+        )}
       </OnHoverScrollContainer>
     </Col>
   );
